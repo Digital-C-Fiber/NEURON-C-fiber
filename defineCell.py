30e29c18ff279bde97fec534cb1f7a2bcdc9530b
AnnaMaxion <anna.maxion@rwth-aachen.de>
2026-02-18 10:45:04 +0100

    initial upload

--- defineCell.py
+++ defineCell.py
@@ -33,7 +33,10 @@ def insertChannels(axon,condFactor, gPump=-0.0047891, gNav17=0.10664, gNav18=0.2
         #seg.kdrTiger.gbar = 0.018002*condFactor#Grill
         seg.kna.gbar = gKna*condFactor
         
-    h.theta_nakdyn = 0.029#Tigerholm
+    #h.theta_nakdyn = 0.029#Tigerholm
+    h.theta_nakdyn = 0.0435
+    #h.theta_nakdyn = 0.058
+    #h.theta_nakdyn = 1
     #h.theta_naoi = 0.029*condFactor#Grill
     #h.theta_koi = 0.029*condFactor#Grill
 
@@ -47,6 +50,7 @@ def setTemp(axon, tempCelsius):
         seg.nakpump.celsiusT = tempCelsius
         seg.kdr.celsiusT = tempCelsius#Tigerholm
         #seg.kdrTiger.celsiusT = tempCelsius#Grill
+        #seg.nakdyn.celsiusT=tempCelsius
     h.celsiusT_nav1p8 = tempCelsius
 
 #calculates the maximum conductances of the sodium and potassium leak (balancing) currents to achieve their target rest potential (Grill)
@@ -54,18 +58,54 @@ def balance(axon, Vrest):
     inaSum = -(axon.ina_nattxs + axon.ina_nav1p9+axon.ina_nav1p8 + axon.ina_h+axon.ina_nakpump)
     if (inaSum/(Vrest-axon.ena))<0:
         axon.pumpina_extrapump=inaSum
+        #print("Na Extrapump")
     else:
-        axon.gnaleak_leak = inaSum/(Vrest-axon.ena)
+        #axon.gnaleak_leak = inaSum/(Vrest-axon.ena)
+        axon.gnaleak_leak = inaSum
+        #print("Na Leak")
 
     ikSum = -(axon.ik_ks+axon.ik_kf + axon.ik_h + axon.ik_kdr + axon.ik_nakpump + axon.ik_kna)#Tigerholm
     #ikSum = -(axon.ik_ks+axon.ik_kf + axon.ik_h + axon.ik_kdrTiger + axon.ik_nakpump + axon.ik_kna)#Grill
     if (ikSum/(Vrest-axon.ek))<0:
         axon.pumpik_extrapump=ikSum
+        #print("K Extrapump")
     else:
-        axon.gkleak_leak = ikSum/(Vrest-axon.ek)
+        #axon.gkleak_leak = ikSum/(Vrest-axon.ek)
+        axon.gkleak_leak = ikSum
+        #print("K Leak")
        
+    
+def checkBalance(axon):
+    #check extrapump
+    scoreExtraNa = max(axon.ina_nattxs, axon.ina_nav1p9, axon.ina_nav1p8, axon.ina_h, axon.ina_nakpump)-axon.pumpina_extrapump# should be >0
+    if scoreExtraNa < 0:#extrapump is larger than max current
+        scoreExtraNa=abs(scoreExtraNa)# the larger the difference. the larger the score
+    else:
+        scoreExtraNa=0
+    
+    #check leak
+    scoreLeakNa = max(axon.ina_nattxs, axon.ina_nav1p9, axon.ina_nav1p8, axon.ina_h, axon.ina_nakpump)-axon.gnaleak_leak# should be >0
+    if scoreLeakNa < 0:#extrapump is larger than max current
+        scoreLeakNa=abs(scoreLeakNa)# the larger the difference. the larger the score
+    else:
+        scoreLeakNa=0
 
-
+    #check extrapump
+    scoreExtraK = max(axon.ik_ks, axon.ik_kf, axon.ik_h, axon.ik_kdr, axon.ik_nakpump, axon.ik_kna)-axon.pumpik_extrapump# should be >0
+    if scoreExtraK < 0:#extrapump is larger than max current
+        scoreExtraK=abs(scoreExtraK)# the larger the difference. the larger the score
+    else:
+        scoreExtraK=0
+    
+    #check leak
+    scoreLeakK = max(axon.ik_ks, axon.ik_kf, axon.ik_h, axon.ik_kdr, axon.ik_nakpump, axon.ik_kna)-axon.gkleak_leak# should be >0
+    if scoreLeakK < 0:#extrapump is larger than max current
+        scoreLeakK=abs(scoreLeakK)# the larger the difference. the larger the score
+    else:
+        scoreLeakK=0
+        
+    score=(scoreExtraNa+scoreLeakNa+scoreExtraK+scoreLeakK)/4
+    return score
     
     
     
